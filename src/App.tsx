1f9759e794db224f180fbfb31df2c236e036be79
KugelblitZ-121 <amr.ghrbal@agu.edu.tr>
2024-12-19 10:35:22 +0300

    fixing errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./pages/Footer";
 import Poster from "./pages/PosterSection";
 import Section2 from "./pages/TeamSection";
-import Trailer from "./pages/TrailerSection";
+//import Trailer from "./pages/TrailerSection";
 import Gameplay from "./pages/GameplaySection";
 import Comics from "./pages/ComicsSection";
 //import Section6 from "./pages/Section6";
@@ -14,7 +14,7 @@ function App() {
       <NavBar />
       <Poster />
       {/* <div className="w-full h-2"></div> */}
-      <Trailer />
+      {/* <Trailer /> */}
       <Gameplay />
       <Comics />
       <Section2 />
